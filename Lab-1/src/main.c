ffc28516bcc87cf00a5b06159041288d79a01898
Doshinsky <akter1997@gmail.com>
2018-10-04 10:54:29 +0700

    Added prime input checking

--- Lab-1/src/main.c
+++ Lab-1/src/main.c
@@ -5,6 +5,25 @@
 #include <sys/time.h>
 
 int main(int argc, char const *argv[]){
-  /* code */
+  if (argc < 2){
+    fprintf(stderr, "[ERROR] Not enough arguments!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (!strcmp(argv[1], "-f")){
+    if (argc < 3){
+      fprintf(stderr, "[ERROR] No input file was found!\n");
+      exit(EXIT_FAILURE);
+    }
+  } else if (!strcmp(argv[1], "-s")){
+    if (argc < 3){
+      fprintf(stderr, "[ERROR] No input stream was detected!\n");
+      exit(EXIT_FAILURE);
+    }
+  }else{
+    fprintf(stderr, "[ERROR] Unknow parameter!\n");
+    exit(EXIT_FAILURE);
+  }
+
   return EXIT_SUCCESS;
 }
