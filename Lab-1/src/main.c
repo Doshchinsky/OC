06ad9a05213d73f4361426d05ac9079e9f701269
Doshinsky <akter1997@gmail.com>
2018-10-04 10:31:12 +0700

    Created samples and .gitignore

---
+++ Lab-1/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <sys/time.h>
+
+int main(int argc, char const *argv[]){
+  /* code */
+  return EXIT_SUCCESS;
+}
